4a3cc5e77cb5a9a977d5640b84f3b7d14a34f79b
Brayden Wilder <braydenwilder@gmail.com>
2022-09-02 12:47:06 -0600

    index.html

--- java/script.js
+++ java/script.js
@@ -1,85 +1,112 @@
 // X Create a default 16 x 16 grid
 // X Create a hover effect to change cell color
-// Create a way to change grid size
-// Make the new grid replace the default
-// Set grid limit to 100
-// Make the grid stay the same size
+// X Create a way to change grid size
+// X Make the new grid replace the default
+// X Set grid limit to 100
+// X Make the grid stay the same size
 // Make a RGB hover option
-// Make a 10% black per pass option
+// Make an eraser option
 
+const defaultSize = 16;
+const defaultMode = 'color';
+const defaultColor = '#333333';
 
+let currentSize = defaultSize;
+let currentMode = defaultMode;
+let currentColor = defaultColor;
 
-const myGrid = document.getElementById('myGrid');
-let rows = document.getElementsByClassName('gridRow');
-let cells = document.getElementsByClassName('cell');
-const container = document.getElementById('container');
-
-container.appendChild(myGrid);
-
-
-let slider = document.getElementById('myRange');
-let output = document.getElementById('slideval');
-output.textContent = '16 x 16';
-
-
-
-
-
-
-function defaultGrid() {
-    makeRows(16);
-    makeColumns(16);
+function setCurrentSize(newSize) {
+    currentSize = newSize;
 }
 
-function makeRows(rowNum) {
-    for(r = 0; r < rowNum; r++) {
-        let row = document.createElement('div');
-        myGrid.appendChild(row).className = 'gridRow';
-    };
-};
+function setCurrentMode(newMode) {
+    currentMode = newMode;
+}
 
-function makeColumns(cellNum) {
-    
-    for(i = 0; i < rows.length; i++) {
-        for(j = 0; j < cellNum; j++) {
-            let newCell = document.createElement('div');
-            rows[j].appendChild(newCell).className = 'cell';
-        };
-    };
-};
+function setCurrentColor(newColor) {
+    currentColor = newColor;
+}
 
-slider.oninput = function() {
+const colorPicker = document.getElementById('colorPicker');
+const colorBtn = document.getElementById('color');
+const rainbowBtn = document.getElementById('rainbow');
+const eraserBtn = document.getElementById('eraser');
+const clearBtn = document.getElementById('clear');
+const grid = document.getElementById('grid');
+const sizeSlider = document.getElementById('sizeSlider');
+const sizeValue = document.getElementById('sizeValue');
 
-    if(slider.value === '1') {
-        myGrid.innerHTML = '';
-        defaultGrid();
-        return output.textContent = '16 x 16';
+colorPicker.oninput = (e) => setCurrentColor(e.target.value)
+colorBtn.onclick = () => setCurrentMode('color');
+rainbowBtn.onclick = () => setCurrentMode('rainbow');
+eraserBtn.onclick = () => setCurrentMode('eraser');
+clearBtn.onclick = () => reloadGrid();
+sizeSlider.onmousemove = (e) => updateSizeValue(e.target.value);
+sizeSlider.onchange = (e) => changeSize(e.target.value);
+
+let mouseDown = false;
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
+
+function changeSize(value) {
+    setCurrentSize(value);
+    updateSizeValue(value);
+    reloadGrid();
+}
+
+function updateSizeValue(value) {
+    sizeValue.textContent = `${value} x ${value}`;
+}
+
+function reloadGrid() {
+    clearGrid();
+    setupGrid(currentSize);
+}
+
+function clearGrid() {
+    grid.innerHTML = '';
+}
+
+function setupGrid(size) {
+    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+    for(i = 0; i < size * size; i++) {
+        const gridElement = document.createElement('div');
+        gridElement.classList.add('grid-element');
+        gridElement.addEventListener('mouseover', changeColor);
+        gridElement.addEventListener('mousedown', changeColor);
+        grid.appendChild(gridElement);
     }
-    else if(slider.value === '2') {
-        output.textContent = '32 x 32';
-        myGrid.innerHTML = '';
-        makeRows(32);
-        makeColumns(32);
-        return;
-    }
-    else if(slider.value === '3') {
-        return output.textContent = '48 x 48';
-    }
-    else if(slider.value === '4') {
-        return output.textContent = '64 x 64';
-    }
-    else if(slider.value === '5') {
-        return output.textContent = '80 x 80';
-    }
-    else if(slider.value === '6') {
-        return output.textContent = '96 x 96'
+
+}
+
+function changeColor(e) {
+    if(e.type === 'mouseover' && !mouseDown) return;
+    if(currentMode === 'rainbow') {
+        const randomR = Math.floor(Math.random() * 256);
+        const randomG = Math.floor(Math.random() * 256);
+        const randomB = Math.floor(Math.random() * 256);
+
+        e.target.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`
+    } else if(currentMode === 'color') {
+        e.target.style.backgroundColor = currentColor;
+    } else if(currentMode === 'eraser') {
+        e.target.style.backgroundColor = '#fefefe';
     }
 }
 
-defaultGrid();
+function activateButton(newMode) {
+    if(currentMode === 'rainbow') {
+        rainbowBtn.classList.remove('active');
+    } else if(currentMode === 'color') {
+        colorBtn.classList.remove('active');
+    } else if(currentMode === 'eraser') {
+        eraserBtn.classList.remove('active');
+    }
+}
 
-for(i = 0; i < cells.length; i++) {
-    cells[i].addEventListener('mouseover', function(event) {
-        event.target.style.backgroundColor = 'black';
-    })
+window.onload = () => {
+    setupGrid(defaultSize);
+    activateButton(defaultMode);
 }
